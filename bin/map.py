5aaec26402f488385ac1a5e5fbb720e6a36288d1
John Bowman <bowman@ualberta.ca>
2018-03-11 23:35:56 -0600

    Update to latest rbx.

--- bin/map.py
+++ bin/map.py
@@ -10,9 +10,8 @@ rZ=re.compile("Z:"+numeric,re.VERBOSE)
 rdelta=re.compile("delta:"+numeric,re.VERBOSE)
 
 Length=200
-Width=150
-
-ygap=11
+Width=149
+ygap=8
 
 X=Length
 Y=Width-ygap
@@ -29,26 +28,21 @@ yoffset=ygap+(Y-(ny-1)*my)/2
 z=0
 
 def wait():
-    global z
-    output=""
-    time.sleep(1)
     while True:
-        line=str(subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M114"]))
-#        sys.stdout.write(line+"\n")
-        z=float(rZ.findall(str(line))[0])
-        if line == output and z <= 0.2:
+        line=str(subprocess.check_output(["rbx","status"]))
+        if re.match("b'Printer idle",line):
             break
-        time.sleep(0.5)
-        output=line
+        else:
+            sys.stdout.write(line+"\n")
     return
 
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M139"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 Z4"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 Z2"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 X105Y75"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","T0"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M107"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M190"])
+subprocess.check_output(["rbx","gcode","M139"])
+subprocess.check_output(["rbx","gcode","G0 Z4"])
+subprocess.check_output(["rbx","gcode","G0 Z2"])
+subprocess.check_output(["rbx","gcode","G0 X105Y75"])
+subprocess.check_output(["rbx","gcode","T0"])
+subprocess.check_output(["rbx","gcode","M107"])
+#subprocess.check_output(["rbx","gcode","M190"])
 
 for i in range(nx):
     sys.stdout.write(str(xoffset+i*mx)+" ")
@@ -58,17 +52,18 @@ for j in range(ny):
 sys.stdout.write("\n")
 
 def probe(x0,y0):
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 Z2"])
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 X"+str(x0)+"Y"+str(y0)])
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G28 Z"])
-    wait()
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 Z2"])
-    return subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M113"])
+    subprocess.check_output(["rbx","gcode","G0 Z2"])
+    subprocess.check_output(["rbx","gcode","G0 X"+str(x0)+"Y"+str(y0)])
+    subprocess.check_output(["rbx","gcode","G28 Z"])
+#    wait()
+    subprocess.check_output(["rbx","gcode","G0 Z2"])
+    sys.stdout.write(str(subprocess.check_output(["rbx","gcode","M113"]))+"\n")
+    return subprocess.check_output(["rbx","gcode","M113"])
 
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G39"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 X105Y75"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G28 Z"])
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G39 S1.0"])
+subprocess.check_output(["rbx","gcode","G39"])
+subprocess.check_output(["rbx","gcode","G0 X105Y75"])
+subprocess.check_output(["rbx","gcode","G28 Z"])
+subprocess.check_output(["rbx","gcode","G39 S1.0"])
 
 Z0=[0]*nx
 n=4
@@ -80,8 +75,8 @@ for j in range(n):
         line=probe(x0,yoffset)
         z0 += float(rdelta.findall(str(line))[0])
         Z0[i] += z0
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G0 X105Y75"])
-    subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","G28 Z"])
+    subprocess.check_output(["rbx","gcode","G0 X105Y75"])
+    subprocess.check_output(["rbx","gcode","G28 Z"])
 
 for i in range(nx):
     Z0[i] /= n
@@ -98,4 +93,4 @@ for i in range(nx):
         sys.stdout.write(str(z0)+" ")
         sys.stdout.flush()
     sys.stdout.write("\n")
-subprocess.check_output(["sudo","/u/bowman/bin/rbx","gcode","M139 S0"])
+subprocess.check_output(["rbx","gcode","M139 S0"])
